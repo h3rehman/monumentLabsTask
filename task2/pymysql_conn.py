347a14fc0fff314b6502b5016c30a0638c0860ce
h3rehman <h3rehman@gmail.com>
2018-05-29 00:51:41 -0500

    Update pymysql_conn.py

--- task2/pymysql_conn.py
+++ task2/pymysql_conn.py
@@ -18,13 +18,16 @@ try:
 
     credentials = {}
     bulk_User = []
+    
+    #getting each user data into a directory and appending that into the bulk_User list
     for row in data:
         credentials['user_id']= row[0]
         credentials['name']= str(row[1])
         credentials['email']= str(row[2])
         bulk_User.append(credentials)
         credentials = {}
-
+    
+    #doing a bulk insert/creation job
     intercom.users.submit_bulk_job(create_items=bulk_User)    
     #print (bulk_User)
     print('Data transmitted')
